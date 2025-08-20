726c7db53e26d3c77487adc1083be90edee4d25f
Tiffany Campbell <tcampbell7573@floridapoly.edu>
2025-08-20 18:42:44 +0000

    Created the first c++ program

---
+++ 01_c++_intro/first_program.cpp
@@ -0,0 +1,22 @@
+/*
+* 08/20/2025
+* The first c++ program
+*
+*/
+
+#include <iostream>
+
+using namespace std; 
+
+using std::cout;
+
+//Create a main function
+int main(void){
+    std::cout << "Hello COP3337!" << std::endl;
+
+    //std - standard namespace 
+    //:: - scope resolution operator
+    //cout - console output (objest of an iostream class)
+    // << stream insertion operator
+    return 0;
+}
