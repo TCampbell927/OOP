9eaf2a68ea89a406cf176d149fcf72b6b4f74fcd
Tiffany Campbell <tcampbell7573@floridapoly.edu>
2025-08-20 18:43:19 +0000

    Created to learn namespaces

---
+++ 01_c++_intro/namespaces.cpp
@@ -0,0 +1,21 @@
+//create the namespace
+#include <iostream>
+namespace first {
+    int a = 5;
+    void print () {
+        std::cout << "First" << std::endl;
+    }
+}
+
+namespace second {
+    int a = 5;
+    void print () {
+        std::cout << "Second" << std::endl;
+    }
+}
+
+int main(void) {
+    first::print();
+    second::print();
+    return 0;
+}
